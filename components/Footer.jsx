062e07dca49ba6712da09fd4a74b4107482de3f9
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-27 14:06:02 +0700

    modified: fixed Footer & ScrollToTop components

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="main min-h-max">
+    <footer className="main min-h-max mb-[84px]">
       <div className="container border-t-2 border-gray-300 py-8 text-center dark:border-t-gray-800">
         <h1 className="font-bold text-gray-900 dark:text-white">
           @ Copyrights {new Date().getFullYear()} - Develop by ❤️
--- components/ScrollToTop.jsx
+++ components/ScrollToTop.jsx
@@ -1,35 +1,35 @@
-import { useEffect, useState } from 'react';
-import { RiArrowUpLine } from 'react-icons/ri';
+import { useEffect, useState } from "react";
+import { RiArrowUpLine } from "react-icons/ri";
 
 export default function ScrollToTop() {
-  const [scrollVisible, setScrollVisible] = useState('-bottom-full');
+  const [scrollVisible, setScrollVisible] = useState("-bottom-full");
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScrollVisible);
+    window.addEventListener("scroll", handleScrollVisible);
 
     return () => {
-      window.removeEventListener('scroll', handleScrollVisible);
+      window.removeEventListener("scroll", handleScrollVisible);
     };
   }, [scrollVisible]);
 
   function handleScrollToTop() {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   }
 
   function handleScrollVisible() {
     if (window.scrollY > 400) {
-      setScrollVisible('bottom-12');
+      setScrollVisible("bottom-24");
     } else {
-      setScrollVisible('-bottom-full');
+      setScrollVisible("-bottom-full");
     }
   }
 
   return (
     <div
-      className={`fixed right-6 z-50 cursor-pointer rounded-md bg-green-600 p-3 text-[1.3rem] text-white shadow-md transition-all duration-300 ${scrollVisible}`}
+      className={`fixed w-[38px] h-[38px] flex justify-center items-center right-6 z-20 cursor-pointer rounded-md bg-green-600 text-[1.3rem] text-white shadow-md transition-all duration-300 ${scrollVisible}`}
       onClick={handleScrollToTop}
     >
       <RiArrowUpLine />
