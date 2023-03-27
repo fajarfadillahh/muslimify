6b93d20f88d10e3521c0cfd9ee24a7dcf9727fa6
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-27 14:55:10 +0700

    modified: added active class at the Navbar components

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaQuran, FaPrayingHands, FaStarAndCrescent } from "react-icons/fa";
 
 // import components
 import Link from "next/link";
 
 const Navbar = () => {
+  const [navbarActive, setNavbarActive] = useState("/");
+
+  const handleNavbarActive = (link) => {
+    setNavbarActive(link);
+  };
+
   return (
     <nav className="fixed inset-x-0 bottom-0 z-50 mx-auto flex h-[84px] max-w-[480px] items-center justify-center bg-white shadow-[0_-6px_15px_rgba(0,0,0,0.1)] transition dark:bg-gray-900">
       <div className="container">
@@ -18,7 +24,10 @@ const Navbar = () => {
               <Link
                 href={link}
                 key={text}
-                className="navbar-active inline-flex flex-col items-center gap-1 text-gray-600 dark:text-gray-500"
+                className={`navbar-active inline-flex flex-col items-center gap-1 text-gray-600 cursor-pointer dark:text-gray-500 ${
+                  navbarActive === link ? "active" : ""
+                }`}
+                onClick={() => handleNavbarActive(link)}
               >
                 <span className="text-[1.5rem]">{icon}</span>
                 <p className="text-[14px] font-extrabold">{text}</p>
