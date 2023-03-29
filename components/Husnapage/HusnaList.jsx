75ba9d14d91e8d0a58da638001ca7922460d4b61
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-29 17:42:50 +0700

    create: HusnaList parts - Husnapage

---
+++ components/Husnapage/HusnaList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HusnaList = () => {
+  return (
+    <section className="section pt-12">
+      <div className="container grid grid-cols-[repeat(2,max-content)] justify-center gap-8">
+        <h1>husna card</h1>
+      </div>
+    </section>
+  );
+};
+
+export default HusnaList;
--- pages/husna.jsx
+++ pages/husna.jsx
@@ -1,12 +1,14 @@
 // import components
 import Layout from "@/components/Layout";
 import Hero from "@/components/Husnapage/Hero";
+import HusnaList from "@/components/Husnapage/HusnaList";
 
 export default function husna() {
   return (
     <>
       <Layout>
         <Hero />
+        <HusnaList />
       </Layout>
     </>
   );
