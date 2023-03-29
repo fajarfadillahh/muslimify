9dfe03cfd34f27672c377a762d124d727ad5c6c1
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-29 17:37:52 +0700

    create: Husna page & Hero parts - Husnapage

---
+++ pages/husna.jsx
@@ -0,0 +1,13 @@
+// import components
+import Layout from "@/components/Layout";
+import Hero from "@/components/Husnapage/Hero";
+
+export default function husna() {
+  return (
+    <>
+      <Layout>
+        <Hero />
+      </Layout>
+    </>
+  );
+}
