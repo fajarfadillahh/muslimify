6a3a5989bec6272a616161fe3d1695422eb8df75
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-29 17:58:38 +0700

    modified: get data asmaul-husna from api

--- components/Husnapage/HusnaList.jsx
+++ components/Husnapage/HusnaList.jsx
@@ -3,11 +3,13 @@ import React from "react";
 // import components
 import HusnaCard from "./HusnaCard";
 
-const HusnaList = () => {
+const HusnaList = ({ dataHusna }) => {
   return (
     <section className="section pt-12">
       <div className="container grid grid-cols-[repeat(2,max-content)] justify-center gap-8">
-        <HusnaCard />
+        {dataHusna?.map((husna) => {
+          return <HusnaCard key={husna.number} asmaulHusna={husna} />;
+        })}
       </div>
     </section>
   );
--- pages/husna.jsx
+++ pages/husna.jsx
@@ -3,13 +3,26 @@ import Layout from "@/components/Layout";
 import Hero from "@/components/Husnapage/Hero";
 import HusnaList from "@/components/Husnapage/HusnaList";
 
-export default function husna() {
+export default function husna({ dataHusna }) {
   return (
     <>
       <Layout>
         <Hero />
-        <HusnaList />
+        <HusnaList dataHusna={dataHusna} />
       </Layout>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://apimuslimify.vercel.app/api/v2/asmaulhusna"
+  );
+  const result = await response.json();
+
+  return {
+    props: {
+      dataHusna: result.data,
+    },
+  };
 }
