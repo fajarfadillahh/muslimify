862dd941f3516d5598fc96e91fb965dcd81c7950
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-29 18:01:37 +0700

    modified: comsume data asmaul-husna on the HusnaCard - Husnapage

--- components/Husnapage/HusnaCard.jsx
+++ components/Husnapage/HusnaCard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const HusnaCard = () => {
+const HusnaCard = ({ asmaulHusna }) => {
+  const { number, arabic, latin, id_translation } = asmaulHusna;
+
   return (
     <div className="group h-max w-[175px] rounded-md bg-white shadow-md transition hover:ring-2 hover:ring-green-600">
       <div className="grid gap-4 p-6">
         <span className="inline-flex h-[28px] w-[28px] items-center justify-center rounded-full bg-green-600 text-[12px] font-bold text-white">
-          1
+          {number}
         </span>
 
         <div className="grid gap-2 text-center">
@@ -13,15 +15,15 @@ const HusnaCard = () => {
             dir="rtl"
             className="truncate text-[28px] font-bold text-gray-900 rtl:mr-0"
           >
-            arabic
+            {arabic}
           </h1>
 
           <div className="overflow-hidden">
             <h3 className="truncate whitespace-nowrap text-[14px] font-extrabold text-green-600">
-              latin
+              {latin}
             </h3>
             <p className="text-[12px] text-gray-600 line-clamp-2 group-hover:line-clamp-none">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              {id_translation}
             </p>
           </div>
         </div>
