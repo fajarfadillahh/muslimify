822e915e5324eed2a5e28157d7c1b9996e4ddcdf
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-31 11:46:16 +0700

    modified: fixed PrayerAccordion parts - Prayer page

--- components/Prayerpage/PrayerList.jsx
+++ components/Prayerpage/PrayerList.jsx
@@ -1,12 +1,30 @@
 // import components
+import { useState } from "react";
 import PrayerAccordion from "./PrayerAccordion";
 
 const PrayerList = ({ dataPrayer }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleIsOpen = (index) => {
+    if (isOpen === index) {
+      return setIsOpen(null);
+    }
+
+    setIsOpen(index);
+  };
+
   return (
     <section className="section pt-12">
       <div className="container grid gap-4">
         {dataPrayer?.map((prayer, index) => {
-          return <PrayerAccordion key={index} prayer={prayer} />;
+          return (
+            <PrayerAccordion
+              key={index}
+              prayer={prayer}
+              isOpen={isOpen === index}
+              handleIsOpen={() => handleIsOpen(index)}
+            />
+          );
         })}
       </div>
     </section>
