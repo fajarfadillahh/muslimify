449d244bd57ba3912925503ab1b01631efcc8b72
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-31 11:08:29 +0700

    create: Hero parts - Prayer page

--- pages/prayer.jsx
+++ pages/prayer.jsx
@@ -1,10 +1,13 @@
 // import components
 import Layout from "@/components/Layout";
+import Hero from "@/components/Prayerpage/Hero";
 
 export default function prayer() {
   return (
     <>
-      <Layout></Layout>
+      <Layout>
+        <Hero />
+      </Layout>
     </>
   );
 }
