a19a0d3d7b41c11519e0ed276b79eb321a9221b5
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-31 11:33:34 +0700

    modified: get data prayer from api

--- pages/prayer.jsx
+++ pages/prayer.jsx
@@ -3,13 +3,26 @@ import Layout from "@/components/Layout";
 import Hero from "@/components/Prayerpage/Hero";
 import PrayerList from "@/components/Prayerpage/PrayerList";
 
-export default function prayer() {
+export default function prayer({ dataPrayer }) {
   return (
     <>
       <Layout>
         <Hero />
-        <PrayerList />
+        <PrayerList dataPrayer={dataPrayer} />
       </Layout>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://apimuslimify.vercel.app/api/v2/doaharian"
+  );
+  const result = await response.json();
+
+  return {
+    props: {
+      dataPrayer: result.data,
+    },
+  };
 }
