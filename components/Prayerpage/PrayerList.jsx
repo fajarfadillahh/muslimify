55d04eb7c615fbe87470d0ce0f4ea5612f82517a
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-31 11:10:53 +0700

    create: PrayerList parts - Prayer page

---
+++ components/Prayerpage/PrayerList.jsx
@@ -0,0 +1,11 @@
+const PrayerList = () => {
+  return (
+    <section className="section pt-12">
+      <div className="container grid gap-4">
+        <h1>prayer accordion</h1>
+      </div>
+    </section>
+  );
+};
+
+export default PrayerList;
--- pages/prayer.jsx
+++ pages/prayer.jsx
@@ -1,12 +1,14 @@
 // import components
 import Layout from "@/components/Layout";
 import Hero from "@/components/Prayerpage/Hero";
+import PrayerList from "@/components/Prayerpage/PrayerList";
 
 export default function prayer() {
   return (
     <>
       <Layout>
         <Hero />
+        <PrayerList />
       </Layout>
     </>
   );
